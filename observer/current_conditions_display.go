be7c7381e8d5c05d30c700bf2f22efb80730814a
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-04-15 10:24:29 +0900

    Add current conditions display struct

---
+++ observer/current_conditions_display.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// CurrentConditionsDisplay implement Observer & DisplayElement
+type CurrentConditionsDisplay struct {
+	temperature float64
+	humidity    float64
+	weatherData Subject
+}
+
+func NewCurrentConditionsDisplay(weatherData Subject) *CurrentConditionsDisplay {
+	cd := &CurrentConditionsDisplay{}
+
+	cd.weatherData = weatherData
+	weatherData.RegisterObserver(cd)
+	return cd
+}
+
+func (cd *CurrentConditionsDisplay) Update(temperature, humidity, pressure float64) {
+	cd.temperature = temperature
+	cd.humidity = humidity
+	cd.Display()
+}
+
+func (cd *CurrentConditionsDisplay) Display() {
+	fmt.Printf("Current conditions: %f F degrees and %f 5 humidity", cd.temperature, cd.humidity)
+}
