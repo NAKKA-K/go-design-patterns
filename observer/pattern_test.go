e59666a5a9a2f972faa3bd953d46aa92de453057
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-04-15 18:05:39 +0900

    Add test code

--- observer/pattern_test.go
+++ observer/pattern_test.go
@@ -3,4 +3,16 @@ package main
 import "testing"
 
 func TestObserver(t *testing.T) {
+	weatherData := NewWeatherData()
+
+	data := interface{}(weatherData).(Subject)
+
+	_ = NewCurrentConditionsDisplay(data)
+	_ = NewStatisticsDisplay(*weatherData)
+	_ = NewForecastDisplay(*weatherData)
+	_ = NewHeatIndexDisplay(*weatherData)
+
+	weatherData.SetMeasurements(80, 65, 30.4)
+	weatherData.SetMeasurements(82, 70, 29.2)
+	weatherData.SetMeasurements(78, 90, 29.2)
 }
